863db4adeba599a52257f0a1ee602ab5f91b4984
KoxNoob <lmevistre@gmail.com>
2022-12-05 17:53:09 +0100

    test2

--- comparateur-benchmark.py
+++ comparateur-benchmark.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
 
+#toto
+
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0'}
 
 # Compétition football
